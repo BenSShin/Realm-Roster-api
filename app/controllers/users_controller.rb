8b7290ecb299401188fe3363d9d84343e4a2d677
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-12-20 14:10:52 -0600

    created crud actions for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,48 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render :show
+  end
+
+  def create
+    if params[:password] == params[:password_confirmation]
+      @user = User.new(
+        username: params[:username],
+        email: params[:email],
+        profile_picture: params[:profile_picture],
+        password: params[:password],
+        password_confirmation: params[:password_confirmation],
+        group_id: params[:group_id],
+      )
+      if @user.save
+        render :show
+      else
+        render json: { errors: @user.errors.full_messages }, status: :bad_request
+      end
+    else
+      render json: { message: "Password and password_confirmation don't match" }
+    end
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.update(
+      username: params[:username] || @user.username,
+      email: params[:email] || @user.email,
+      profile_picture: params[:profile_picture] || @user.profile_picture,
+      group_id: params[:group_id] || @user.group_id,
+    )
+    render :show
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+    render json: { message: "User has been deleted" }
+  end
 end
--- app/views/users/_user.json.jbuilder
+++ app/views/users/_user.json.jbuilder
@@ -1,2 +1,7 @@
-json.extract! user, :id, :created_at, :updated_at
-json.url user_url(user, format: :json)
+json.id user.id
+json.username user.username
+json.email user.email
+json.profile_picture user.profile_picture
+json.group_id user.group_id
+json.created_at user.created_at
+json.updated_at user.updated_at
