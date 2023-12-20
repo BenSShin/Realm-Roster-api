4339a07b26d300cfb420d51b0ac9d64976669a86
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-12-20 14:32:53 -0600

    added group model

---
+++ db/migrate/20231220203207_create_groups.rb
@@ -0,0 +1,11 @@
+class CreateGroups < ActiveRecord::Migration[7.0]
+  def change
+    create_table :groups do |t|
+      t.string :name
+      t.datetime :meetup
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
