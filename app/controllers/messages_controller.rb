8308dd73adc860f349f1642638fce8b48c0dd6d5
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-12-20 15:59:39 -0600

    crud message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,38 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user
+
+  def index
+    @messages = Message.where(group_id: current_user.group_id)
+    render :index
+  end
+
+  def show
+    @message = Message.find_by(id: params[:id])
+    render :show
+  end
+
+  def create
+    @message = Message.create(
+      user_id: current_user.id,
+      group_id: current_user.group_id,
+      content: params[:content],
+    )
+    render :show
+  end
+
+  def update
+    @message = Message.find_by(id: params[:id])
+    @message.update(
+      user_id: current_user.id,
+      group_id: current_user.group_id,
+      content: params[:content] || @message.content,
+    )
+    render :show
+  end
+
+  def destroy
+    @message = Message.find_by(id: params[:id])
+    @message.delete
+    render json: { message: "Message deleted." }
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_20_203207) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_20_214326) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_20_203207) do
     t.string "name"
     t.datetime "meetup"
     t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "group_id"
+    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
