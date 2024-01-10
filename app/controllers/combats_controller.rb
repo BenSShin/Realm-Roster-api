58dd4850d5f7d361999e5ecaac41fd688d5abb10
ben <ben.shin27@gmail.com>
2024-01-10 00:02:06 -0600

    added actions to combat controller

--- app/controllers/combats_controller.rb
+++ app/controllers/combats_controller.rb
@@ -1,12 +1,52 @@
 class CombatsController < ApplicationController
   before_action :authenticate_user
 
-  def index 
-    @combats = Combat.where(user_id: current_user.id && tab_id: params[:tab_id])
+  def index
+    @combats = Combat.where(user_id: current_user.id, tab_id: params[:tab_id])
     render :index
   end
-  
-  
-  
-  
+
+  def show
+    @combat = Combat.find_by(user_id: current_user.id, id: params[:id])
+    render :show
+  end
+
+  def create
+    @combat = Combat.new(
+      user_id: current_user.id,
+      tab_id: params[:tab_id],
+      creature_name: params[:creature_name],
+      initiative_roll: params[:initiative_roll],
+      health: params[:health],
+      status: params[:status],
+    )
+    if @group.save
+      render :show
+    else
+      render json: { errors: @group.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def update
+    @combat = Combat.find_by(user_id: current_user.id, id: params[:id])
+    @combat.update(
+      user_id: current_user.id,
+      tab_id: @combat.tab_id,
+      creature_name: params[:creature_name] || @combat.creature_name,
+      initiative_roll: params[:initiative_roll] || @combat.initiative_roll,
+      health: params[:health] || @combat.health,
+      status: params[:status] || @combat.status,
+    )
+    if @group.save
+      render :show
+    else
+      render json: { errors: @group.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def destroy
+    @combat = Combat.find_by(user_id: current_user.id, id: params[:id])
+    @combat.destroy
+    render json: { message: "card was deleted" }
+  end
 end
